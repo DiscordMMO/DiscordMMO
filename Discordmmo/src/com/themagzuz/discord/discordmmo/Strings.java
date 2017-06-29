d70e633518685ab182b5ecbfc880dc94e75b7745
TheMagzuz <markus.dam123@gmail.com>
2017-06-29 19:13:40 +0200

    Added constant for when the player is not registered

---
+++ Discordmmo/src/com/themagzuz/discord/discordmmo/Strings.java
@@ -0,0 +1,8 @@
+package com.themagzuz.discord.discordmmo;
+
+public final class Strings
+{
+
+	public static final String NOT_REGISTERED = ": You are not registered. Register with $register [name]";
+	
+}
